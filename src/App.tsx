b95ed48a555de15b3405fc3d7842743b562fad54
Swathi Bali <baliswathi519@gmail.com>
2024-07-11 00:02:33 +0530

    some common advanced props

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,20 @@
 import './App.css'
 import { Greet } from "./components/Greet";
+import { Heading } from './components/Heading';
+import { Oscar } from './components/Oscar';
 import { Person } from "./components/Person";
 import { PersonList } from './components/PersonList';
+import { Status } from './components/Status';
 
 function App() {
-
-  const personName= {
-    first: 'Bruce',
-    last: 'Wayne'
-  }
-
-  const nameList = [
-    {
-      first: "Bruce",
-      last: "Wayne",
-    },
-    {
-      first: "Clark",
-      last: "Kent",
-    },
-    {
-      first: "Princess",
-      last: "Diana",
-    },
-  ];
   return (
     <div className="App">
-      <Greet name={"Swathi"} messageCount={10} isLoggedIn={false}/>
-      <Person name = {personName}/>
-      <PersonList names={nameList} />
+      <Status status="loading" />
+      <Heading>Placeholder text</Heading>
+      <Oscar>
+        <Heading>Oscar text</Heading>
+      </Oscar>
+      <Greet name='Swathi' isLoggedIn={false}/>
     </div>
   );
 }
