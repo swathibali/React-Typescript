42a8c991efa44024510c59feaa1fc84524489f8b
Swathi Bali <baliswathi519@gmail.com>
2024-07-16 08:23:14 +0530

    Style Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import './App.css'
-import { Button } from './components/Button';
-import { Greet } from "./components/Greet";
-import { Heading } from './components/Heading';
-import { Input } from './components/Input';
-import { Oscar } from './components/Oscar';
-import { Person } from "./components/Person";
-import { PersonList } from './components/PersonList';
-import { Status } from './components/Status';
+import './App.css';
+import { Container } from './components/Container';
 
 function App() {
   return (
     <div className="App">
-        <Button handleClick={(event, id)=>{
-          console.log('Button Clicked', event, id)
-        }} />
-        <Input value='' handleChange = { event => console.log(event)} />
+        <Container styles={{ border: '1px solid black', padding: '1rem'}} />
     </div>
   );
 }
