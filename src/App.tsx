d137d49291b88bd4d8e64a0188977910e49126b6
Swathi Bali <baliswathi519@gmail.com>
2024-07-17 19:59:20 +0530

    useReducer Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import './App.css';
 import { Container } from './components/Container';
+import { Counter } from './components/state/Counter';
 import { User } from './components/state/User';
 
 function App() {
   return (
     <div className="App">
-       <User />
+      <Counter />
     </div>
   );
 }
