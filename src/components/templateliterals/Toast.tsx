a6e363e68d1cc05692b4135eb47fb6ae3832f530
Swathi Bali <baliswathi519@gmail.com>
2024-07-25 08:50:42 +0530

    Template Listeral and Exclude

---
+++ src/components/templateliterals/Toast.tsx
@@ -0,0 +1,13 @@
+type HorizontalPosition = "left" | "center" | "right";
+
+type VerticalPosition = "top" | "center" | "bottom";
+
+type ToastProps = {
+  position:
+    | Exclude<`${HorizontalPosition}-${VerticalPosition}`, "center-center">
+    | "center";
+};
+
+export const Toast = ({ position }: ToastProps) => {
+  return <div>ToastcNotification Position - {position}</div>;
+};
